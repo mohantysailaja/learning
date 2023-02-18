8cedf702107eae5994fe5149b6bd97f58baaf192
Sailaja Mohanty <sailaja.mohanty@workday.com>
2023-02-18 21:39:52 +0000

    new file method

---
+++ learningJava/src/methodjava.java
@@ -0,0 +1,9 @@
+public class methodjava {
+    static void myMethod() {
+        System.out.println("Hello World!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+    }
+}
